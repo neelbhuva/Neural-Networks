89fbf4cc916a28dccc21e167f2341af55b9e49ca
neelbhuva <neel.7365@gmail.com>
2017-09-26 21:43:14 -0400

    Complete

--- Project 1/parity.py
+++ Project 1/parity.py
@@ -1,6 +1,29 @@
 import itertools
 import numpy as np
 import math
+import random as rd
+
+def initialize_weights():
+	weights = []
+	for i in range(0,num_units):
+		weights.append([])
+		for j in range(0,4):
+			rand = rd.random()
+			flag = rd.choice([0,1])
+			if(flag == 0):
+				rand = rand * (-1)
+			weights[i].append(rand)
+	return weights
+
+def initialize_bias():
+	weights = []
+	for i in range(0,num_units):
+		rand = rd.random()
+		flag = rd.choice([0,1])
+		if(flag == 0):
+			rand = rand * (-1)
+		weights.append(rand)
+	return weights
 
 def get_sigmoid(x,a):
 	return 1 / (1 + math.exp((-1) * a * x))
@@ -26,13 +49,15 @@ def desiredOutputs(lst):
 	return des_output
 
 def activate(v_n):
+	#print("In activate")
 	y_n = []
 	for i in v_n:
+		#print(i)
 		y_n.append(get_sigmoid(i,1))
 	return y_n
 
 def get_error(d_n,y_n):
-	return d_n - y_n
+	return (d_n - y_n)
 
 def get_delta_k(error_out,v_n_out):
 	return (error_out * get_sigmoid_prime(v_n_out,1))
@@ -42,42 +67,81 @@ def out_weight_update(w_n,b_n,learn_rate,error_out,v_n_out,y_n):
 	b_n_plus_one = 0
 	for i in range(0,len(w_n)):
 		w_n_plus_one.append(w_n[i] + learn_rate * get_delta_k(error_out,v_n_out) * y_n[i])
-	b_n_plus_one = b_n + learn_rate * get_delta_k(error_out,v_n_out) * get_sigmoid(1,1)
+	b_n_plus_one = b_n + learn_rate * get_delta_k(error_out,v_n_out) * 1
 	return (w_n_plus_one,b_n_plus_one)
 
 def hidden_weight_update(w_n,b_n,learn_rate,v_n,x,delta_k):
 	w_n_plus_one = []
 	b_n_plus_one = []
 	for i in range(0,num_hidden_units):
+		w_n_plus_one.append([])
 		for j in range(0,len(w_n[i])):
-			w_n_plus_one.append(w_n[i][j] + learn_rate * get_sigmoid_prime(v_n[i],1) * delta_k * w_n[4][i] * x[i])
-		b_n_plus_one.append(b_n[i] + learn_rate * get_sigmoid_prime(v_n[i],1) * delta_k * b_n[4] * 1)
+			w_n_plus_one[i].append(w_n[i][j] + learn_rate * get_sigmoid_prime(v_n[i],1) * delta_k * w_n[4][i] * x[j])
+		b_n_plus_one.append(b_n[i] + learn_rate * get_sigmoid_prime(v_n[i],1) * delta_k * w_n[4][i] * 1)
 	return(w_n_plus_one,b_n_plus_one)
+
+def isDesiredError(error,stop_error):
+	for i in error:
+		if(i > stop_error):
+			return False
+	return True
+
+def initializeWithOne(size):
+	temp = []
+	for i in range(0,size):
+		temp.append(1)
+	return temp
 
 def learn(train_data,des_output,learn_rate,weights,b):
 	stop_error = 0.05
-	error = 1
-	while(error > stop_error):
+	size = 16
+	error = initializeWithOne(size)
+	epochs = 0
+	while(not(isDesiredError(error,stop_error))):
+		#print(weights)
 		for p in range(0,len(train_data)):
+			#print("---------------------------Training data : " + str(p))
 			v_n = []
 			v_n_out = []
 			for i in range(0,num_hidden_units):
 				v_n.append(get_v_of_n(train_data[p],weights[i],b[i]))
-			y_n = activate(v_n)
+			y_n = activate(v_n)			
 			#print(y_n)
 			for i in range(num_hidden_units,num_hidden_units + num_out_units):
-				v_n_out.append(get_v_of_n(y_n,weights[i],b[i]))
+				v_n_out.append(get_v_of_n(y_n,weights[4],b[4]))
 			y_n_out = activate(v_n_out) 
 			#print(y_n_out)
 			error_out = get_error(des_output[p],y_n_out[0])
+			error[p] = abs(error_out)
 			w_n_plus_one,b_n_plus_one = out_weight_update(weights[4],b[4],learn_rate,error_out,v_n_out[0],y_n)
+			
 			delta_k = get_delta_k(error_out,v_n_out[0])
 			w,b1 = hidden_weight_update(weights,b,learn_rate,v_n,train_data[p],delta_k)
-			weights[4] = w_n_plus_one
+			# print("w n plus one : ")
+			# print(w_n_plus_one)
+			# print("b_n_plus_one : ")
+			# print(b_n_plus_one)
+			# print(w)
+			# print(b1)
+			for i in range(0,len(w_n_plus_one)):
+				weights[4][i] = w_n_plus_one[i]
 			b[4] = b_n_plus_one
-			weights[1:3] = w
-			b[1:3] = b1
-		print(error)
+			
+			for i in range(0,num_hidden_units):
+				for j in range(0,4):
+					weights[i][j] = w[i][j]
+				b[i] = b1[i]
+			# print(weights)
+			# print(b)
+			#print("---------------------------------------------------")
+		epochs = epochs + 1
+		if(epochs % 1000 == 0):
+			print(epochs)
+			print("Error : " + str(error))
+	print("Number of epochs : " + str(epochs))
+	print("Error : " + str(error))
+	print(weights)
+	print(b)
 if __name__ == '__main__':
 	global num_hidden_units
 	num_hidden_units = 4
@@ -89,10 +153,16 @@ if __name__ == '__main__':
 	lst = list(itertools.product([0, 1], repeat=n))
 	des_output = desiredOutputs(lst)
 	print(lst)
-	print(des_output)
+	#print(des_output)
 	v_n = get_v_of_n(lst[2],lst[3],1)
-	print(v_n)
+	#print("v_n :" + str(v_n))
 	learn_rate = 0.5
-	init_weights = [(-1,-1,-1,0),(0,-1,-1,1),(1,1,0,-1),(0,0,1,1),(-1,0,-1,-1)]
-	init_b = (0,1,1,-1,0)
+	alpha = 0.9
+	#learn_rate = learn_rate/(1-alpha)
+	init_weights = initialize_weights()
+	#init_weights = np.random.rand(5,4)
+	print(init_weights)
+	init_b = initialize_bias()
+	#init_b = np.random.rand(5)
+	print(init_b)
 	learn(lst,des_output,learn_rate,init_weights,init_b)
